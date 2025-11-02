3a1a1176b636305befa3669affe62222f5ca7267
emibyte <fevymarine@gmail.com>
2025-11-02 18:08:09 +0100

    repo setup

---
+++ bin/main.ml
@@ -0,0 +1,3 @@
+let () = print_endline "Hello, World!"
+
+let add a b = a +. b
---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "ocaml flake";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            ocamlPackages.ocaml
+            ocamlPackages.dune_3
+
+            ocamlPackages.findlib
+            ocamlPackages.utop
+            ocamlPackages.odoc
+            ocamlPackages.ocaml-lsp
+            ocamlformat
+          ];
+        };
+      });
+    };
+}
